6d05adc87a40ab621e615da79174207b153b38fb
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-06-22 21:30:33 -0400

    Update TestMyDate.java
    change

--- TestMyDate.java
+++ TestMyDate.java
@@ -21,4 +21,15 @@ class MyDate {
         this.month = month;
         this.day = day;
     }
+  public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
 }
