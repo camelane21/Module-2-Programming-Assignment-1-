50248e5d29560673f129e7574f12ed771836cc6f
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-06-22 21:53:51 -0400

    Update main.java

--- main.java
+++ main.java
@@ -40,3 +40,12 @@ class MyDate {
         this.day = calendar.get(GregorianCalendar.DAY_OF_MONTH);
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        MyDate date1 = new MyDate();
+        MyDate date2 = new MyDate(34355555133101L);
+
+        System.out.println("Date 1: " + date1.getYear() + "-" + (date1.getMonth() + 1) + "-" + date1.getDay());
+        System.out.println("Date 2: " + date2.getYear() + "-" + (date2.getMonth() + 1) + "-" + date2.getDay());
+    }
+}
